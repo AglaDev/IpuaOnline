5fbdd224e2ceabd3b82c199c0bf5def59a68d305
ZeAglailson <jaglailson1@gmail.com>
2020-06-29 03:22:14 -0300

    menu iniciado

--- js/main.js
+++ js/main.js
@@ -2,20 +2,23 @@ window.onload = ()=>{
 
 	//Animações do botão do menu
 
-(function btnMenu(){ 
+(function Menu(){ 
 
-	var btnMenu = document.getElementById('btn-menu')
+	let btnMenu = document.getElementById('btn-menu')
+	let menu = document.querySelector('.content-menu')
 
-	openCloseMenu(btnMenu)	
+	openCloseMenu(btnMenu,menu)	
 
 }())
 
 //função responsavel de abrir e fechar o menu e mudar o botão
-function openCloseMenu(btnmenu){
+function openCloseMenu(btnmenu,menu){
 
 	btnmenu.addEventListener('click',()=>{
 
 		btnmenu.classList.toggle('btn-menu-toggle')
+
+		menu.classList.toggle('content-menu-toggle')
 
 
 
